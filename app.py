e51167670607d7e2e0330e7db3cb23627fe37abd
charlie8179 <whirlmore@gmail.com>
2025-04-24 11:27:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,5 @@ def handle_challenge():
     return jsonify({"challengeResponse": hashed}), 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
-if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
