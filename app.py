4d126f85c98d3689e589bb1b06cda281d98a2ffe
charlie8179 <whirlmore@gmail.com>
2025-04-24 10:40:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -21,3 +21,5 @@ def handle_challenge():
 
 if __name__ == "__main__":
     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
